c0bd43d55a583a66fca4e3ca3a0ab538cfdd6659
mart458n <75613058+mart458n@users.noreply.github.com>
2020-12-07 11:07:03 +0100

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript: MonoBehaviour
+{
+    public float maxSpeed = 10.0f;
+    public bool faceRight = true;
+
+    Animator anim;
+    private Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("movementSpeed", Mathf.Abs(move));
+        rigid.velocity = new Vector2(move * maxSpeed, rigid.velocity.y);
+
+        if((move > 0 && !faceRight) || (move < 0 && faceRight)) 
+        {
+            FlipFacing();
+        }
+    }
+
+    void FlipFacing() 
+    {
+        faceRight = !faceRight;
+        Vector3 charScale = transform.localScale;
+        charScale.x = -1 * charScale.x;
+        transform.localScale = charScale;
+    }
+}
